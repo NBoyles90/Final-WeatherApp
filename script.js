2c5d6ba62a64a35b06b76ff1a0e53e0db0190df3
NBoyles90 <nikki.boyles@ymail.com>
2021-05-18 20:27:32 -0500

    updated comments and made minor clean up fixes

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var app = new Vue({
     //global variables that can be used anywhere on the html
     data: {
         link: "",
-        //display variables
+        //display variables - 
         userZip: "",
         userDegree: "",
         temp: "",
@@ -20,20 +20,22 @@ var app = new Vue({
         feels: "",
         humidity: "",
         tempMax: "",
-        tempMin: "" ,
-        bigDegree: "",
-        lilDegree: "",     
+        tempMin: ""  
     },
     //this function is to be used many times so it could not just go into mounted because it would not beable to be called again by the button
     methods: {
         updateAPI(){
+            //creates the link to use and update when the user inputs data
             this.link = `http://api.openweathermap.org/data/2.5/weather?zip=${this.userZip},us&units=${this.userDegree}&appid=8593489cf78cb16629c8045243ffbc9c`;
             this.useAPI();
         },
         useAPI(){
+            //does all the hard work for us, gets the data, packages it up into usable object from json and confirms vailid response
             axios.get(this.link)
                 .then((response) => {
+                    //empties the message variable
                     this.errMessage = "";
+                    //gathers all the information for the variables to be filled and placed in the HTML
                     console.log(response.data)
                     this.temp = Math.ceil(response.data.main.temp);
                     this.city = response.data.name;
@@ -43,25 +45,25 @@ var app = new Vue({
                     this.tempMax = Math.ceil(response.data.main.temp_max);
                     this.tempMin = Math.ceil(response.data.main.temp_min);
                     this.visDescription = response.data.weather[0].description; 
+                    //catches the error if the user enters a zip that is not available in the API 
+                    //prints instructions to enter a valid zipcode
                 }).catch((error) => {
                     this.errMessage = "*This is not a Valid Zipcode in the US*";
                 })
         },
         validateZip(){
-            try{
-                if((this.userZip.length == 5)&&(this.userZip > 0)) {
-                    this.errMessage = "";
-                    this.updateAPI();
-                }
-                else{
-                    this.errMessage = "*Please enter a Valid Zipcode composed of 5 digits*"
-                }   
+            //only allows the user to enter 5 characters and they have to be greater than 0 so it ensures no Alpha characters
+            if((this.userZip.length == 5)&&(this.userZip > 0)) {
+                //empties the message variable
+                this.errMessage = "";
+                this.updateAPI();
             }
-            catch(err){
-                alert(err)
-                this.errMessage = err
-            }
+            else{
+                //catches the error if the user's text does not meet the conditions in the if statement
+                this.errMessage = "*Please enter a Valid Zipcode composed of 5 digits*"
+            }             
         },
+        //is called when user clicks on the unit of measurement (f/c) and updates the userDegree with the values returned
         displayDegree(){
             console.log(this.userDegree)
             this.updateAPI()
@@ -69,6 +71,7 @@ var app = new Vue({
     },
     // a function that runs when the page is loaded, it calls the function useAPI to populate the information 
     mounted: function mounted() {
+        //starter information for when the page first loads
         this.userZip = "65807"
         this.userDegree = "imperial"
         this.validateZip()
